517f2bead0fd894c5f1f346f38bc5ac035d98414
Sariah Campopiano <scampopiano925@gmail.com>
2022-04-30 15:51:22 -0500

    Created HTML elements with unique IDs. Created cardsOnDom function which houses the template of how each card will render to the DOM

---
+++ scripts/data.js
@@ -0,0 +1,44 @@
+const students = [
+  {
+    id: 1,
+    studentImg: "http://images6.fanpop.com/image/photos/43400000/Harry-Potter-fan-art-harry-potter-43424936-500-613.jpg",
+    name: "Harry Potter",
+    houseImg: "https://i.pinimg.com/originals/01/fe/8b/01fe8b73e3660609f0f0785c4d7e9ef6.jpg",
+    houseName: "Gryffindor",
+    expelled: false
+  },
+  {
+    id: 2,
+    studentImg: "https://static0.srcdn.com/wordpress/wp-content/uploads/2020/06/Ron-Weasley-Is-Our-King-Harry-Potter-Fan-Art.jpg?q=50&fit=crop&w=740&h=888&dpr=1.5",
+    name: "Ron Weasley",
+    houseImg: "https://i.pinimg.com/originals/01/fe/8b/01fe8b73e3660609f0f0785c4d7e9ef6.jpg",
+    houseName: "Gryffindor",
+    expelled: false
+  },
+  {
+    id: 3,
+    studentImg: "https://preview.redd.it/btoxqxzch3881.png?auto=webp&s=39423bef48cc8d0dd02e2cdece5fab9277daa686",
+    name: "Hermione Granger",
+    houseImg: "https://i.pinimg.com/originals/01/fe/8b/01fe8b73e3660609f0f0785c4d7e9ef6.jpg",
+    houseName: "Gryffindor",
+    expelled: false
+  },
+  {
+    id: 4,
+    studentImg: "https://cdnb.artstation.com/p/assets/images/images/026/914/031/large/maria-hideki-.jpg?1592162734",
+    name: "Cedric Diggory",
+    houseImg: "https://i.pinimg.com/originals/15/0f/2b/150f2b856ea49796ddd9d14cc7607a86.jpg",
+    houseName: "Hufflepuff",
+    expelled: false
+  },
+  {
+    id: 5,
+    studentImg: "https://img.wattpad.com/8f7e233550cbb1dbf09f20c7ac18b4ad59e4db89/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f6646665f57566248396c73784c513d3d2d3138322e313632336435363664613738663237353135303339303032313337392e6a7067?s=fit&w=720&h=720",
+    name: "Luna Lovegood",
+    houseImg: "https://bookstr.com/wp-content/uploads/2020/03/Ravenclaw-colors-I-T-768x480.jpg",
+    houseName: "Ravenclaw",
+    expelled: false
+  }
+];
+
+export default students;
