62090c485c48505e973b307288fd196f1f099597
Sariah Campopiano <scampopiano925@gmail.com>
2022-05-02 15:38:21 -0500

    Tried to get expelled students in another div, but having difficulties in doing so. Need to push up the branch. Come back togit add -A

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,14 @@
-import cardsOnDom from './cardsOnDom.js';
+// import cardsOnDom from './cardsOnDom.js';
 import students from './data.js';
 import sortingHat from './sortingHat.js';
 import founders from './founders.js';
 import schoolSong from './schoolSong.js';
 import welcomeToHogwarts from './welcome.js';
 import housesBtn from './housesBtn.js';
+import deathEaters from './deathEatersOnDom.js';
+
+const expelled = students.expelled;
+console.log(expelled);
 
 
 const startApp = () => {
@@ -13,7 +17,8 @@ const startApp = () => {
   schoolSong();
   sortingHat();
   housesBtn ();
-  cardsOnDom(students);
+  // cardsOnDom(students);
+  deathEaters(students);
 }
 
 startApp();
